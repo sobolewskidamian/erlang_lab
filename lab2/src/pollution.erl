c608de5b3b1960e6d28369bc91a1e10504016d72
Damian Sobolewski <damiansobolewski12@gmail.com>
2019-04-09 22:22:29 +0200

    poprawki

--- lab2/src/pollution.erl
+++ lab2/src/pollution.erl
@@ -25,7 +25,7 @@ existStation([_ | T], Name, {X, Y}) -> existStation(T, Name, {X, Y}).
 
 addStation(Name, {X, Y}, Monitor) ->
   case existStation(maps:keys(Monitor), Name, {X, Y}) of
-    true -> io:format("Taka stacja juz istnieje~n"), Monitor;
+    true -> {error, "Taka stacja juz istnieje"};
     false -> Monitor#{#station{name = Name, coords = #coords{x = X, y = Y}} => []}
   end.
 
@@ -35,7 +35,7 @@ existMeasurement({Date, Time}, Type, [#measurement{date = Date, time = Time, typ
 existMeasurement({Date, Time}, Type, [_ | T]) -> existMeasurement({Date, Time}, Type, T).
 
 
-getStation(_, []) -> io:format("Brak stacji~n");
+getStation(_, []) -> {error, "Brak stacji"};
 getStation({coords, X, Y}, [H | T]) -> case H of
                                          #station{coords = {coords, X, Y}} -> H;
                                          _ -> getStation({coords, X, Y}, T)
@@ -48,12 +48,12 @@ getStation(Name, [H | T]) -> case H of
 
 addValue({X, Y}, {Date, Time}, Type, Value, Monitor) ->
   case existStation(maps:keys(Monitor), n, {X, Y}) of
-    false -> io:format("Taka stacja nie istnieje~n"), Monitor;
+    false -> {error, "Taka stacja juz istnieje"};
     true ->
       ActStation = getStation({coords, X, Y}, maps:keys(Monitor)),
       ActMeasurements = maps:get(ActStation, Monitor),
       case existMeasurement({Date, Time}, Type, ActMeasurements) of
-        true -> io:format("Takie dane juz istnieja~n"), Monitor;
+        true -> {error, "Takie dane juz istnieja"};
         false ->
           Monitor#{ActStation := ActMeasurements ++ [#measurement{date = Date, time = Time, type = Type, value = Value}]}
       end
@@ -61,39 +61,44 @@ addValue({X, Y}, {Date, Time}, Type, Value, Monitor) ->
 
 addValue(Name, {Date, Time}, Type, Value, Monitor) ->
   case existStation(maps:keys(Monitor), Name, {x, y}) of
-    false -> io:format("Taka stacja nie istnieje~n"), Monitor;
+    false -> {error, "Taka stacja juz istnieje"};
     true ->
       ActStation = getStation(Name, maps:keys(Monitor)),
       ActMeasurements = maps:get(ActStation, Monitor),
       case existMeasurement({Date, Time}, Type, ActMeasurements) of
-        true -> io:format("Takie dane juz istnieja~n"), Monitor;
+        true -> {error, "Takie dane juz istnieja"};
         false ->
           Monitor#{ActStation := ActMeasurements ++ [#measurement{date = Date, time = Time, type = Type, value = Value}]}
       end
   end.
 
 
-getMeasurement(_, _, []) -> io:format("Pomiar nie istnieje");
+getMeasurement(_, _, []) -> {error, "Pomiar nie istnieje"};
 getMeasurement({Date, Time}, Type, [H | T]) -> case H of
                                                  #measurement{date = Date, time = Time, type = Type} -> H;
                                                  _ -> getMeasurement({Date, Time}, Type, T)
                                                end.
 
-
 removeValue({X, Y}, {Date, Time}, Type, Monitor) -> case existStation(maps:keys(Monitor), n, {X, Y}) of
-                                                      false -> io:format("Stacja nie istnieje"), Monitor;
+                                                      false -> {error, "Stacja nie istnieje"};
                                                       true ->
                                                         ActStation = getStation({coords, X, Y}, maps:keys(Monitor)),
                                                         ActMeasurements = maps:get(ActStation, Monitor),
-                                                        Monitor#{ActStation := ActMeasurements -- [getMeasurement({Date, Time}, Type, ActMeasurements)]}
+                                                        case existMeasurement({Date, Time}, Type, ActMeasurements) of
+                                                          true -> Monitor#{ActStation := ActMeasurements -- [getMeasurement({Date, Time}, Type, ActMeasurements)]};
+                                                          false -> {error, "Pomiar nie istnieje"}
+                                                        end
                                                     end;
 
 removeValue(Name, {Date, Time}, Type, Monitor) -> case existStation(maps:keys(Monitor), Name, {x, y}) of
-                                                    false -> io:format("Stacja nie istnieje"), Monitor;
+                                                    false -> {error, "Stacja nie istnieje"};
                                                     true ->
                                                       ActStation = getStation(Name, maps:keys(Monitor)),
                                                       ActMeasurements = maps:get(ActStation, Monitor),
-                                                      Monitor#{ActStation := ActMeasurements -- [getMeasurement({Date, Time}, Type, ActMeasurements)]}
+                                                      case existMeasurement({Date, Time}, Type, ActMeasurements) of
+                                                        true -> Monitor#{ActStation := ActMeasurements -- [getMeasurement({Date, Time}, Type, ActMeasurements)]};
+                                                        false -> {error, "Pomiar nie istnieje"}
+                                                      end
                                                   end.
 
 
@@ -103,13 +108,13 @@ getValue({Date, Time}, Type, [_ | T]) -> getValue({Date, Time}, Type, T).
 
 getOneValue({X, Y}, {Date, Time}, Type, Monitor) ->
   case existStation(maps:keys(Monitor), n, {X, Y}) of
-    false -> io:format("Stacja nie istnieje"), Monitor;
+    false -> {error, "Stacja nie istnieje"};
     true -> getValue({Date, Time}, Type, maps:get(getStation({coords, X, Y}, maps:keys(Monitor)), Monitor))
   end;
 
 getOneValue(Name, {Date, Time}, Type, Monitor) ->
   case existStation(maps:keys(Monitor), Name, {x, y}) of
-    false -> io:format("Stacja nie istnieje"), Monitor;
+    false -> {error, "Stacja nie istnieje"};
     true -> getValue({Date, Time}, Type, maps:get(getStation(Name, maps:keys(Monitor)), Monitor))
   end.
 
@@ -121,26 +126,26 @@ countStationMean(Type, [_ | T], {Sum, Amount}) -> countStationMean(Type, T, {Sum
 
 getStationMean({X, Y}, Type, Monitor) ->
   case existStation(maps:keys(Monitor), n, {X, Y}) of
-    false -> io:format("Stacja nie istnieje"), Monitor;
+    false -> {error, "Stacja nie istnieje"};
     true ->
       ActStation = getStation({coords, X, Y}, maps:keys(Monitor)),
       ActMeasurements = maps:get(ActStation, Monitor),
       {Sum, Amount} = countStationMean(Type, ActMeasurements, {0, 0}),
       case Amount of
-        0 -> io:format("Brak pomiarow"), 0;
+        0 -> {error, "Brak pomiarow"};
         _ -> Sum / Amount
       end
   end;
 
 getStationMean(Name, Type, Monitor) ->
   case existStation(maps:keys(Monitor), Name, {x, y}) of
-    false -> io:format("Stacja nie istnieje"), Monitor;
+    false -> {error, "Stacja nie istnieje"};
     true ->
       ActStation = getStation(Name, maps:keys(Monitor)),
       ActMeasurements = maps:get(ActStation, Monitor),
       {Sum, Amount} = countStationMean(Type, ActMeasurements, {0, 0}),
       case Amount of
-        0 -> io:format("Brak pomiarow"), 0;
+        0 -> {error, "Brak pomiarow"};
         _ -> Sum / Amount
       end
   end.
@@ -155,7 +160,7 @@ getDailyMean(Date, Type, Monitor) ->
   ActMeasurements = lists:append(maps:values(Monitor)),
   {Sum, Amount} = countStationMeanDaily(Type, Date, ActMeasurements, {0, 0}),
   case Amount of
-    0 -> io:format("Brak pomiarow"), 0;
+    0 -> {error, "Brak pomiarow"};
     _ -> Sum / Amount
   end.
 
@@ -169,30 +174,24 @@ countDailyDataCount([#measurement{date = Date, type = Type, value = Value} | T],
 countDailyDataCount([_ | T], Map, AmountOfUpdates) -> countDailyDataCount(T, Map, AmountOfUpdates).
 
 getDailyAverageDataCount(Name, Monitor) ->
-  ActStation = getStation(Name, maps:keys(Monitor)),
-  ActMeasurements = maps:get(ActStation, Monitor),
-  {A, AmountOfUpdates} = countDailyDataCount(ActMeasurements, #{}, 0),
-  AmountOfDays = length(maps:keys(A)),
-  case AmountOfDays of
-    0 -> io:format("Brak pomiarow"), 0;
-    _ -> AmountOfUpdates / AmountOfDays
+  case existStation(maps:keys(Monitor), Name, {x, y}) of
+    false -> {error, "Stacja nie istnieje"};
+    true -> ActStation = getStation(Name, maps:keys(Monitor)),
+      ActMeasurements = maps:get(ActStation, Monitor),
+      {A, AmountOfUpdates} = countDailyDataCount(ActMeasurements, #{}, 0),
+      AmountOfDays = length(maps:keys(A)),
+      case AmountOfDays of
+        0 -> {error, "Brak pomiarow"};
+        _ -> AmountOfUpdates / AmountOfDays
+      end
   end.
 
 
 
 
 test() ->
-  M = pollution:createMonitor(),
-  M1 = pollution:addStation("Dupa", {1, 2}, M),
-  M2 = pollution:addStation("Dupaa", {2, 3}, M1),
-  M3 = addValue({1, 2}, calendar:local_time(), "typ", 10, M2),
-  M4 = addValue({1, 2}, calendar:local_time(), "typ2", 20, M3),
-  M5 = addValue("Dupa", calendar:local_time(), "typ3", 30, M4),
-  M6 = addValue("Dupaa", calendar:local_time(), "typ", 12, M5),
-  M7 = addValue("Dupa", {{2019, 3, 27}, {0, 10, 23}}, "typ", 12, M6),
-  M8 = addValue("Dupa", {{2019, 3, 26}, {0, 10, 23}}, "typ", 12, M7),
-  M8.
-
-
-%pollution:getOneValue({1,2},{{2019,3,27},{23,24,19}},"typ",M).
-%pollution:getOneValue("Dupa",{{2019,3,27},{23,24,19}},"typ",M).
+  P = pollution:createMonitor(),
+  P1 = pollution:addStation("Aleja Slowackiego", {50.2345, 18.3445}, P),
+  P2 = pollution:addValue({50.2345, 18.3445}, calendar:local_time(), "PM10", 59, P1),
+  P3 = pollution:addValue("Aleja Slowackiego", calendar:local_time(), "PM2.5", 113, P2),
+  P3.
